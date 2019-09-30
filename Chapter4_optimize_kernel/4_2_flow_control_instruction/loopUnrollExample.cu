b1a5d1985aa3e76b8da8d74fab1f8a59c1c90c5d
Irwin-Liu <lswbblue@163.com>
2019-09-30 17:43:36 +0800

    revise format

--- Chapter4_optimize_kernel/4_2_flow_control_instruction/loopUnrollExample.cu
+++ Chapter4_optimize_kernel/4_2_flow_control_instruction/loopUnrollExample.cu
@@ -5,22 +5,22 @@ __device__ void foo(int *p1, int *p2) {
 // no argument specified, loop will be completely unrolled
 #pragma unroll
 for (int i = 0; i < 12; ++i)
-  p1[i] += p2[i]*2;
+  p1[i] += p2[i] * 2;
    
 // unroll value = 8
 #pragma unroll (X+1)
 for (int i = 0; i < 12; ++i)
-  p1[i] += p2[i]*4;
+  p1[i] += p2[i] * 4;
  
 // unroll value = 1, loop unrolling disabled
 #pragma unroll 1
 for (int i = 0; i < 12; ++i)
-  p1[i] += p2[i]*8;
+  p1[i] += p2[i] * 8;
  
 // unroll value = 4
 #pragma unroll (T2::value)
 for (int i = 0; i < 12; ++i)
-  p1[i] += p2[i]*16;
+  p1[i] += p2[i] * 16;
 }
  
 __global__ void bar(int *p1, int *p2) {
