777a82c0a8ee31b940408462d0be7c7567ef6a49
Irwin-Liu <lswbblue@163.com>
2019-09-30 16:47:39 +0800

    add comments

--- Chapter1_cuda_basics/1_3_GPU_memory/constantMemoryExample.cu
+++ Chapter1_cuda_basics/1_3_GPU_memory/constantMemoryExample.cu
@@ -1,6 +1,6 @@
-__constant__ char con_p[16];
-__constant__ int  con_t[16] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
-__constant__ int  num = 16;
+__constant__ int  num = 16; // method 1
+__constant__ int  con_t[16] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}; // method 1
+__constant__ char con_p[16]; // method 2
 
 __global__ static void constantMemoryDemo(char* result)
 {
@@ -18,7 +18,7 @@ int main(int argc, char** argv[])
   cudaMalloc((void **)&d_result, 16 * sizeof(char));
 
   char h_con_p[16] = "Let's learn cuda";
-  cudaMemcpyToSymbol(con_p, h_con_p, 16 * sizeof(char));
+  cudaMemcpyToSymbol(con_p, h_con_p, 16 * sizeof(char)); // method 2
   constantMemoryDemo<<<1, 32>>>(d_result);
   cudaMemcpy(&h_result, d_result, 16 * sizeof(char), cudaMemcpyDeviceToHost);
 
